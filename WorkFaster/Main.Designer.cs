363a0a5d54d0671f0f0ee4664c859395e65b3eb5
Lobster <dmdago@gmail.com>
2023-10-07 20:12:12 -0300

    Add traybar icon and context menu on tray to exit

--- WorkFaster/Main.Designer.cs
+++ WorkFaster/Main.Designer.cs
@@ -28,13 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Button callApiButton;
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             callApiButton = new Button();
+            notifyIcon = new NotifyIcon(components);
+            apiKeylbl = new Label();
+            apiTokenlbl = new Label();
+            idListlbl = new Label();
+            apiKeytxt = new TextBox();
+            apiTokentxt = new TextBox();
+            idListtxt = new TextBox();
             SuspendLayout();
             // 
             // callApiButton
             // 
-            callApiButton.Location = new Point(585, 157);
+            callApiButton.Location = new Point(565, 324);
             callApiButton.Name = "callApiButton";
             callApiButton.Size = new Size(188, 58);
             callApiButton.TabIndex = 0;
@@ -42,19 +50,97 @@
             callApiButton.UseVisualStyleBackColor = true;
             callApiButton.Click += button1_Click;
             // 
+            // notifyIcon
+            // 
+            notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon.BalloonTipText = "Running";
+            notifyIcon.BalloonTipTitle = "WorkAid Status";
+            notifyIcon.Icon = (Icon)resources.GetObject("notifyIcon.Icon");
+            notifyIcon.Text = "WorkAid";
+            notifyIcon.MouseDoubleClick += notifyIcon_MouseDoubleClick;
+            // 
+            // apiKeylbl
+            // 
+            apiKeylbl.AutoSize = true;
+            apiKeylbl.Location = new Point(32, 36);
+            apiKeylbl.Name = "apiKeylbl";
+            apiKeylbl.Size = new Size(118, 41);
+            apiKeylbl.TabIndex = 1;
+            apiKeylbl.Text = "Api Key";
+            apiKeylbl.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // apiTokenlbl
+            // 
+            apiTokenlbl.AutoSize = true;
+            apiTokenlbl.Location = new Point(32, 129);
+            apiTokenlbl.Name = "apiTokenlbl";
+            apiTokenlbl.Size = new Size(149, 41);
+            apiTokenlbl.TabIndex = 2;
+            apiTokenlbl.Text = "Api Token";
+            apiTokenlbl.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // idListlbl
+            // 
+            idListlbl.AutoSize = true;
+            idListlbl.Location = new Point(32, 227);
+            idListlbl.Name = "idListlbl";
+            idListlbl.Size = new Size(96, 41);
+            idListlbl.TabIndex = 3;
+            idListlbl.Text = "Id List";
+            // 
+            // apiKeytxt
+            // 
+            apiKeytxt.Location = new Point(241, 36);
+            apiKeytxt.Name = "apiKeytxt";
+            apiKeytxt.Size = new Size(512, 47);
+            apiKeytxt.TabIndex = 4;
+            // 
+            // apiTokentxt
+            // 
+            apiTokentxt.Location = new Point(241, 129);
+            apiTokentxt.Name = "apiTokentxt";
+            apiTokentxt.Size = new Size(512, 47);
+            apiTokentxt.TabIndex = 5;
+            // 
+            // idListtxt
+            // 
+            idListtxt.Location = new Point(241, 227);
+            idListtxt.Name = "idListtxt";
+            idListtxt.Size = new Size(512, 47);
+            idListtxt.TabIndex = 6;
+            // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 434);
+            Controls.Add(idListtxt);
+            Controls.Add(apiTokentxt);
+            Controls.Add(apiKeytxt);
+            Controls.Add(idListlbl);
+            Controls.Add(apiTokenlbl);
+            Controls.Add(apiKeylbl);
             Controls.Add(callApiButton);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Main";
-            Text = "Main";
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "WordAid";
+            Resize += Main_Resize;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button callApiButton;
+        private NotifyIcon notifyIcon;
+        private ContextMenuStrip contextMenuStrip;
+        private Label apiKeylbl;
+        private Label apiTokenlbl;
+        private Label idListlbl;
+        private TextBox apiKeytxt;
+        private TextBox apiTokentxt;
+        private TextBox idListtxt;
     }
 }
--- WorkFaster/Main.cs
+++ WorkFaster/Main.cs
@@ -13,6 +13,23 @@ namespace WorkFaster
         public Main()
         {
             InitializeComponent();
+
+            // Set the initial state of the form to Minimized
+            this.WindowState = FormWindowState.Minimized;
+
+            // Optionally, you can hide the window if you don't want it to appear in the taskbar
+            this.ShowInTaskbar = false;
+
+            // Create ContextMenuStrip
+            contextMenuStrip = new ContextMenuStrip();
+
+            // Add "Exit" option
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit");
+            exitMenuItem.Click += exitMenuItem_Click;
+            contextMenuStrip.Items.Add(exitMenuItem);
+
+            // Assign ContextMenuStrip
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
         }
 
         private async void button1_Click(object sender, EventArgs e)
@@ -82,6 +99,35 @@ namespace WorkFaster
             {
                 return false; // Error handling
             }
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized
+            //hide it from the task bar
+            //and show the system tray icon (represented by the NotifyIcon control)
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            // Clean everything
+            // Pending code
+
+
+            // Close app
+            Application.Exit();
         }
     }
 }
