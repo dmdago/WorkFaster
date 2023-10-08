64f80818ddde2017867ff86fc3755ad0fe39f96e
Lobster <dmdago@gmail.com>
2023-10-07 21:22:28 -0300

    More Changes

--- WorkFaster/Program.cs
+++ WorkFaster/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WorkFaster
@@ -17,13 +18,13 @@ namespace WorkFaster
 
         // Constants
         private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x100;
+        private const int WM_KEYDOWN = 0x0100;
         private const int WM_SYSKEYDOWN = 0x0104;
 
         // Variables for keyboard state
         private static bool ctrlPressed = false;
-        private static bool ctrlAPressed = false;
-
+        private static bool altPressed = false;
+        private static bool shiftPressed = false;
 
         [STAThread]
         static void Main()
@@ -54,25 +55,48 @@ namespace WorkFaster
                 {
                     ctrlPressed = true;
                 }
-                else if (vkCode == 65 && ctrlPressed) // "A"
+                else if (vkCode == 164 || vkCode == 165) // 164 is Left Alt, 165 is Right Alt
                 {
-                    ctrlPressed = false;
-                    ctrlAPressed = true;
+                    altPressed = true;
                 }
-                else if (vkCode == 83 && ctrlAPressed) // "S"
+                else if (vkCode == 160 || vkCode == 161) // 160 is Left Shift, 161 is Right Shift
+                {
+                    shiftPressed = true;
+                }
+                else if (ctrlPressed && altPressed && shiftPressed && vkCode == 83) // CTRL + ALT + SHIFT + S
                 {
                     ctrlPressed = false;
-                    ctrlAPressed = false;
-                    MessageBox.Show("Bingo!");
+                    altPressed = false;
+                    shiftPressed = false;
+                    ShowBingoMessage();
                 }
                 else
                 {
                     ctrlPressed = false;
-                    ctrlAPressed = false;
+                    altPressed = false;
+                    shiftPressed = false;
                 }
             }
 
             return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        private static async void ShowBingoMessage()
+        {
+            MessageBox.Show("Bingo!");
+
+            // Esperar 1 segundo
+            await Task.Delay(1000);
+
+            // Cerrar el MessageBox
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is MessageBox)
+                {
+                    form.Close();
+                    break;
+                }
+            }
         }
 
         // Delegate for the hook callback function
